ac6bfc33ff5852879d317b3e22fb1130760a83ff
nishat <nishatroyhana011@gmail.com>
2022-11-08 11:42:10 +0600

    server basic functionality added

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-
-
-
-
-
-
-
-
-
-
-
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors());
+app.use(express.json())
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.70ddixo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
@@ -17,3 +15,10 @@ client.connect(err => {
   // perform actions on the collection object
   client.close();
 });
+
+app.get('/', (req, res)=>{
+  res.send('running')
+})
+app.listen(port, ()=>{
+  console.log('start')
+})
