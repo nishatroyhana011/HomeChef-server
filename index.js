cdfebb8960eb47d80c001e034fe29465b9c4aae3
nishat <nishatroyhana011@gmail.com>
2022-11-09 10:02:54 +0600

    post req for add a service

--- index.js
+++ index.js
@@ -12,30 +12,37 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.70ddixo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
+async function run() {
 
-  try{
-    const collection = client.db("HomeChef").collection("Services");
-    
-    app.post('/jwt', (req, res)=>{
+  try {
+    const serviceCollection = client.db("HomeChef").collection("Services");
+
+    //token send to server
+    app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2h'})
-      res.send({token})
-  })
- 
- 
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+      res.send({ token })
+    });
+
+    //post req for service
+    app.post('/services', async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service);
+      res.send(result);
+    });
+
   }
-  finally{
+  finally {
 
   }
 }
 run()
-.catch(err => console.log(err))
+  .catch(err => console.log(err))
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.send('running')
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log('start')
 })
