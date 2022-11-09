1d8fa03f7daabd7419be835cf624537b04e05d9a
nishat <nishatroyhana011@gmail.com>
2022-11-09 15:48:41 +0600

    service details  added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json())
@@ -30,6 +30,31 @@ async function run() {
       const result = await serviceCollection.insertOne(service);
       res.send(result);
     });
+
+    //get all service
+    app.get('/services', async (req, res) => {
+      const query = {}
+      const cursor = serviceCollection.find(query)
+      const Services = await cursor.toArray();
+      res.send(Services)
+    });
+
+    //get 3 service in home
+    //const selectedService = await cursor.skip(page*size).limit(size).toArray();
+    app.get('/topservice', async (req, res) => {
+      const query = {}
+      const cursor = serviceCollection.find(query)
+      const Services = await cursor.toArray();
+      res.send(Services)
+    });
+
+    //get a service by id
+    app.get('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}
+      const singleService = await serviceCollection.findOne(query);
+      res.send(singleService)
+  });
 
   }
   finally {
