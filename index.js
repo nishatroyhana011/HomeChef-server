3c2703ca985dc9c6ca054391369ab8f3df43839b
nishat <nishatroyhana011@gmail.com>
2022-11-10 16:21:15 +0600

    jwt verification done

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.70ddixo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: 'No token found' })
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(401).send({ message: 'Wrong token' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 async function run() {
 
   try {
@@ -21,7 +37,7 @@ async function run() {
     //token send to server
     app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
       res.send({ token })
     });
 
@@ -41,10 +57,10 @@ async function run() {
     });
 
     //get 3 service in home
-    //const selectedService = await cursor.skip(page*size).limit(size).toArray();
     app.get('/topservice', async (req, res) => {
-      const query = {}
-      const cursor = serviceCollection.find(query)
+      const query = {};
+      const options = { sort: { _id: -1 } };
+      const cursor = serviceCollection.find(query, options).limit(3);
       const Services = await cursor.toArray();
       res.send(Services)
     });
@@ -64,17 +80,19 @@ async function run() {
       res.send(result);
     });
 
-    
-   // display review by email or service id
-    app.get('/reviews', async (req, res) => {
+
+    // display review by email  
+    app.get('/myreviews', verifyJWT, async (req, res) => {
+
+      const decoded = req.decoded;
+
+      if (decoded.email !== req.query.email) {
+        return res.status(403).send({ message: 'wrong user' })
+      }
       let query = {}
       if (req.query.email) {
         query = {
           email: req.query.email
-        }
-      } else if (req.query.serviceId) {
-        query = {
-          serviceId: req.query.serviceId
         }
       }
       const cursor = reviewCollection.find(query);
@@ -82,37 +100,53 @@ async function run() {
       res.send(reviews)
     });
 
+    //service id
+    app.get('/reviews', async (req, res) => {
+      let query = {}
+      if (req.query.serviceId) {
+        query = {
+          serviceId: req.query.serviceId
+        }
+      }
+      const options = { sort: { _id: -1 } };
+      const cursor = reviewCollection.find(query, options);
+      const reviews = await cursor.toArray();
+      res.send(reviews)
+    });
+
     //delete review
-    app.delete('/reviews/:id', async(req, res) =>{
+    app.delete('/reviews/:id',verifyJWT, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
       const result = await reviewCollection.deleteOne(filter);
       res.send(result);
     });
-    
+
     //get review by id
-    app.get('/update/:id', async(req, res)=>{
+    app.get('/update/:id',verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await reviewCollection.findOne(query);
       res.send(result);
     });
 
-    //update a riview
-    app.put('/reviews/:id', async (req, res) => {
+    //update a review
+    app.put('/reviews/:id',verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const review = req.body;
-      const option = {upsert: true};
+      const option = { upsert: true };
       const updatedReview = {
-          $set: {
-              serviceName: review.name,
-              msg: review.msg
-          }
+        $set: {
+          serviceName: review.name,
+          msg: review.msg
+        }
       }
       const result = await reviewCollection.updateOne(query, updatedReview, option);
       res.send(result);
-  });
+    });
+
+
   }
   finally {
 
